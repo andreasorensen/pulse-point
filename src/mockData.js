14667adb12cc20a3fb52d69dbc41c55d64260c0e
Andrea Sorensen <dreasor2987@icloud.com>
2023-09-11 12:30:41 -0700

    add files for each component

---
+++ src/mockData.js
@@ -0,0 +1,229 @@
+const allArticles = {
+  status: "ok",
+  totalResults: 885,
+  articles: [
+    {
+      source: {
+        id: "wired",
+        name: "Wired",
+      },
+      author: "Paresh Dave",
+      title:
+        "Is Google’s Search Engine Smart or Sneaky? A Trial Court Judge Will Decide",
+      description:
+        "Google’s search dominance is going on trial in the biggest US antitrust case since a crackdown on Big Tech that started in 2019.",
+      url: "https://www.wired.com/story/is-googles-search-engine-smart-or-sneaky-a-trial-court-judge-will-decide/",
+      urlToImage:
+        "https://media.wired.com/photos/64fbb262ad5feafc253f613c/191:100/w_1280,c_limit/Google-Search-Engine-Antitrust-Lawsuit-Business-1638063681.jpg",
+      publishedAt: "2023-09-10T11:00:00Z",
+      content:
+        "A family members hurried Google search for a last-second visa to visit New Zealand recently caused a headacheand provided a timely reminder of why Google faces a landmark US antitrust trial next week… [+3093 chars]",
+    },
+    {
+      source: {
+        id: "wired",
+        name: "Wired",
+      },
+      author: "David Nield",
+      title: "How to Take Back Control of Your Photo and Video Storage",
+      description:
+        "Apple's iCloud and Google Photos make file storage easy—as long as your memories stay on someone else's server. There's another way.",
+      url: "https://www.wired.com/story/how-to-take-back-control-photos-videos/",
+      urlToImage:
+        "https://media.wired.com/photos/64fbb045ce986fc4eb0e5520/191:100/w_1280,c_limit/How-to-Take-Back-Control-of-Your-Photo-and-Video-Storage-Gear-GettyImages-696155135.jpg",
+      publishedAt: "2023-09-10T11:00:00Z",
+      content:
+        "This then leaves you with the question of how to get any new photos and videos you take on your phone off your device and on to your computer, where they can join everything you've just downloaded fr… [+2198 chars]",
+    },
+    {
+      source: {
+        id: "wired",
+        name: "Wired",
+      },
+      author: "Edward Ongweso Jr.",
+      title: "Super Apps Are Terrible for People—and Great for Companies",
+      description:
+        'Apps that offer to "do it all" will subject users to even more exploitation and surveillance, while large tech companies profit.',
+      url: "https://www.wired.com/story/super-app-musk-x-wechat-regulation/",
+      urlToImage:
+        "https://media.wired.com/photos/64fa42075bae312310e8a2d3/191:100/w_1280,c_limit/ideas_superapps_technology_race.jpg",
+      publishedAt: "2023-09-10T11:00:00Z",
+      content:
+        "Recently, Elon Musktold X employees that he wants the platform to become a WeChat-style super app, a one-stop shop for social media, communication, travel, on-demand labor, payments, and more. But th… [+5187 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Gizmodo.com",
+      },
+      author: "Justin Carter",
+      title: "The Haunted Mansion Remake Will Scare Disney+ in October",
+      description:
+        "Back in July, Disney released its remake of Haunted Mansion, which didn’t really light the world on spectral fire when it came out. Part of it was due to other big movies releasing in late July at the time and stealing whatever thunder it could’ve possibly ha…",
+      url: "https://gizmodo.com/haunted-mansion-2023-remake-disney-plus-october-1850823262",
+      urlToImage:
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/9f59896b29c717ae71e070ed6e7d013b.jpg",
+      publishedAt: "2023-09-10T16:00:00Z",
+      content:
+        "Back in July, Disney released its remake of Haunted Mansion, which didnt really light the world on spectral fire when it came out. Part of it was due to other big movies releasing in late July at the… [+1928 chars]",
+    },
+    {
+      source: {
+        id: "bbc-news",
+        name: "BBC News",
+      },
+      author: "https://www.facebook.com/bbcnews",
+      title: "US and Vietnam get a friendship upgrade",
+      description:
+        "The US and Vietnam are becoming better friends - and that's a troubling prospect for China.",
+      url: "https://www.bbc.co.uk/news/world-66748670",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/7981/production/_131050113_gettyimages-1656690718.jpg",
+      publishedAt: "2023-09-10T22:16:09Z",
+      content:
+        "President Joe Biden has denied that the US is attempting to stem China's international influence, after signing a new strategic partnership with Vietnam. \r\nMore than 50 years since the last American … [+8126 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Android Central",
+      },
+      author: "harish.jonnalagadda@futurenet.com (Harish Jonnalagadda)",
+      title:
+        "Review: Stuffcool Ally is a 10,000mAh power bank that charges your Apple Watch",
+      description:
+        "Stuffcool just introduced a 10,000mAh power bank that has a built-in dock to charge the Apple Watch. Combine that with a bundled Lighting cable and dual USB-C ports, and you have the best power bank for iOS devices in India.",
+      url: "https://www.androidcentral.com/accessories/stuffcool-ally-power-bank-review",
+      urlToImage:
+        "https://cdn.mos.cms.futurecdn.net/gbWPhjTjRX8hEYXh4ejTs-1200-80.jpg",
+      publishedAt: "2023-09-10T06:48:26Z",
+      content:
+        "Stuffcool is an Indian accessory maker that's doing all the right things. The brand has an extensive portfolio of chargers and power banks, and I've highlighted several previously, including the 20,0… [+3549 chars]",
+    },
+  ],
+};
+
+
+
+const topHeadlines = {
+  status: "ok",
+  totalResults: 33,
+  articles: [
+    {
+      source: {
+        id: null,
+        name: "Hollywood Reporter",
+      },
+      author: "Caitlin Huston",
+      title:
+        "‘Drew Barrymore Show’ Audience Members Say They Were Kicked Out for Writers Guild Support Amid Picket - Hollywood Reporter",
+      description:
+        "The talk show resumed taping without its three WGA writers on Monday as picketers demonstrated outside of CBS Studios.",
+      url: "https://www.hollywoodreporter.com/tv/tv-news/drew-barrymore-show-audience-kicked-out-1235587064/",
+      urlToImage:
+        "https://www.hollywoodreporter.com/wp-content/uploads/2023/09/IMG_8367-H-2023.jpg?w=1024",
+      publishedAt: "2023-09-11T16:25:37Z",
+      content:
+        "Writers Guild of America members and scribes for the Drew Barrymore show picketed outside CBS Studios Monday in New York as the talk show resumed filming amid the writers and actors strikes. \r\nTwo au… [+5025 chars]",
+    },
+    {
+      source: {
+        id: "reuters",
+        name: "Reuters",
+      },
+      author: "Alexander Cornwell, Ahmed Eljechtimi",
+      title: "Morocco earthquake: Rescuers race to find survivors - Reuters",
+      description:
+        "Villagers wept for their lost relatives in the rubble of their homes on Monday as the death toll from Morocco's deadliest earthquake in more than six decades rose to almost 2,700 and rescuers raced against time to find survivors.",
+      url: "https://www.reuters.com/world/africa/rescuers-race-find-survivors-over-48-hours-after-morocco-quake-2023-09-11/",
+      urlToImage:
+        "https://www.reuters.com/resizer/BPpoRW4jzq9l_gwMWwQ3L1Y9v9k=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/FKGSFZYV7NOJXJMC6FDULWSSIM.jpg",
+      publishedAt: "2023-09-11T16:09:12Z",
+      content:
+        "TALAT N'YAAQOUB, Morocco, Sept 11 (Reuters) - Villagers wept for their lost relatives in the rubble of their homes on Monday as the death toll from Morocco's deadliest earthquake in more than six dec… [+5104 chars]",
+    },
+    {
+      source: {
+        id: "nbc-news",
+        name: "NBC News",
+      },
+      author: "Yuliya Talmazan, Ostap Hunkevych",
+      title:
+        "Ukraine and the U.S. appear at odds over Russia offensive - NBC News",
+      description:
+        "America’s top military commander is warning that Kyiv has limited time to pursue a stagnating military offensive as senior Ukrainian officials vow to push on, possibly putting Ukraine and its top backer, the U.S., at odds.",
+      url: "https://www.nbcnews.com/news/world/ukraine-russia-milley-offensive-winter-weather-g20-rcna104327",
+      urlToImage:
+        "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2023-09/230911-zelenskyy-biden-mb-1040-eb46b6.jpg",
+      publishedAt: "2023-09-11T15:36:30Z",
+      content:
+        "Americas top military commander is warning that Kyiv has limited time to pursue a stagnating military offensive as senior Ukrainian officials vow to push on, possibly putting Ukraine and its top back… [+4224 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "MacRumors",
+      },
+      author: "Joe Rossignol",
+      title:
+        "Apple Event: Recapping Last-Minute Rumors for iPhone 15 and More - MacRumors",
+      description:
+        "We're just one day away from Apple's annual September event, and as usual, there have been several last-minute rumors about the iPhone 15 and...",
+      url: "https://www.macrumors.com/2023/09/11/apple-event-iphone-15-last-minute-rumors/",
+      urlToImage:
+        "https://images.macrumors.com/t/hchU43mD3ubaxFFtrOp7cYfLNqg=/2500x/article-new/2023/09/What-to-Expect-at-Apples-Wonderlust-Event-Feature-2-1.jpg",
+      publishedAt: "2023-09-11T15:08:36Z",
+      content:
+        "We're just one day away from Apple's annual September event, and as usual, there have been several last-minute rumors about the iPhone 15 and other devices. The event begins on Tuesday at 10 a.m. Pac… [+384 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Vulture",
+      },
+      author: "Jason P. Frank",
+      title:
+        "Demi Lovato Revealed as Anonymouse on 'The Masked Singer' - Vulture",
+      description:
+        "‘The Masked Singer’ season 10 premiered on September 10, with a kickoff episode. Featured was “Anonymouse,” a one episode guest who sang “What About Love” and was revealed as Demi Lovato.",
+      url: "http://www.vulture.com/2023/09/demi-lovato-masked-singer-performance.html",
+      urlToImage:
+        "https://pyxis.nymag.com/v1/imgs/035/71b/fbcefbb14909acd2505576a3266c1b57ca-masked-singer-demi-lovato.1x.rsocial.w1200.jpg",
+      publishedAt: "2023-09-11T15:02:34Z",
+      content:
+        "The Masked Singer is back and still slightly scary. It just provokes so many questions, like: Why is anti-vaxxer and non-singer Jenny McCarthy Wahlberg a judge? Why is national treasure Dick Van Dyke… [+1144 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "YouTube",
+      },
+      author: null,
+      title:
+        "Shannon Sharpe says the Cowboys DIDN'T put the league on notice after win over Giants | First Take - ESPN",
+      description:
+        "Shannon Sharpe says the Cowboys DIDN'T put the league on notice after win over Giants 👀 | First TakeStephen A. Smith, Shannon Sharpe and Ryan Clark react to...",
+      url: "https://www.youtube.com/watch?v=0LR7Vcr0bag",
+      urlToImage: "https://i.ytimg.com/vi/0LR7Vcr0bag/maxresdefault.jpg",
+      publishedAt: "2023-09-11T14:54:05Z",
+      content: null,
+    },
+    {
+      source: {
+        id: "reuters",
+        name: "Reuters",
+      },
+      author: "Stephen Nellis",
+      title:
+        "Qualcomm to supply Apple with 5G chips until 2026 under new deal - Reuters",
+      description:
+        'Qualcomm <a href="https://www.reuters.com/markets/companies/QCOM.O" target="_blank">(QCOM.O)</a> on Monday said it had signed a deal with Apple <a href="https://www.reuters.com/markets/companies/AAPL.O" target="_blank">(AAPL.O)</a> to supply 5G chips until at…',
+      url: "https://www.reuters.com/technology/qualcomm-supply-apple-with-5g-chips-until-2026-under-new-deal-2023-09-11/",
+      urlToImage:
+        "https://www.reuters.com/resizer/uO_Sv61sOsaFSTxr85hdbbeRygw=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/GE2LQOIXQ5PXXDDWBDPDSSONHI.jpg",
+      publishedAt: "2023-09-11T14:52:00Z",
+      content:
+        "Sept 11 (Reuters) - Qualcomm (QCOM.O) on Monday said it had signed a deal with Apple (AAPL.O) to supply 5G chips until at least 2026, at a time when the iPhone maker faces increased challenges in Chi… [+2421 chars]",
+    },
+  ]
+};
