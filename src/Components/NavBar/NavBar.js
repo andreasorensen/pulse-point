cc9875e22fa63aef31dbe8ff1119883af8100f9f
Andrea Sorensen <dreasor2987@icloud.com>
2023-09-11 18:08:54 -0700

    Nav Styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 const Navbar = () => {
   const categories = ['business', 'entertainment', 'health', 'science', 'sports', 'technology'];
 
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
+      <NavLink className="nav-button" to="/">Home</NavLink>
       {categories.map(category => (
         <NavLink className='nav-button' key={category} to={`/category/${category}`}>
           {category.charAt(0).toUpperCase() + category.slice(1)}
