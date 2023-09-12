6fa5a42e88dfca21a11a90ced6b2ab6979de2f5d
Andrea Sorensen <dreasor2987@icloud.com>
2023-09-11 17:19:46 -0700

    ArticleDetails component completed

---
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ArticleDetail = ({ articles }) => {
+  const { title } = useParams();
+
+  const article = articles.find(article => article.title === title);
+
+  if (!article) {
+    return <p className='not-found'>Article not found!</p>;
+  }
+
+  return (
+    <div>
+      <h1 className='article-title'>{article.title}</h1>
+      <img className='article-image' src={article.urlToImage} alt={article.title} />
+      <p className='article-date'>{new Date(article.publishedAt).toDateString()}</p>
+      <p className='article-source' >Source: {article.source.name}</p>
+      <p className='content'>{article.content}</p>
+    </div>
+  );
+}
+
+export default ArticleDetail;
